93b233ae28f79028b6980273f8e95a158009ab44
Dmitry Shlepkin <acidl33t@gmail.com>
2025-03-14 14:12:06 -0400

    Initial Commit

---
+++ CVSCodeTask/CVSCodeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSCodeTaskApp.swift
+//  CVSCodeTask
+//
+//  Created by Dmitry Shlepkin on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CVSCodeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVSCodeTaskTests/CVSCodeTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  CVSCodeTaskTests.swift
+//  CVSCodeTaskTests
+//
+//  Created by Dmitry Shlepkin on 3/14/25.
+//
+
+import Testing
+@testable import CVSCodeTask
+
+struct CVSCodeTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
